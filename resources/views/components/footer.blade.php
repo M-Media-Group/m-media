6a99faa3b84f960dcaa122dc9195b8856f970cec
Michał Wargan <mwargan@Michas-MacBook-Pro.local>
2019-08-27 04:47:54 +0200

    first commit

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,9 @@
+		<!-- STAY - Scripts -->
+		<script src="{{ asset('js/manifest.js') }}" defer></script>
+
+		<script src="{{ asset('js/vendor.js') }}" defer></script>
+
+		<script src="{{ asset('js/app.js') }}" defer></script>
 
    		@yield('footer_scripts')
 	</body>
--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -38,12 +38,13 @@
 
 <link rel="icon" href="{{ config('blog.favicon_url') }}">
 
-<!-- STAY - Scripts -->
-<script src="{{ asset('js/manifest.js') }}" defer></script>
-
-<script src="{{ asset('js/vendor.js') }}" defer></script>
-
-<script src="{{ asset('js/app.js') }}" defer></script>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','{{ config('blog.google_tag_id') }}');</script>
+<!-- End Google Tag Manager -->
 
 <!-- Fonts -->
 <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -54,11 +55,3 @@
 <link href="{{ asset('css/all.css') }}" rel="stylesheet">
 
 @yield('header_scripts')
-
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','{{ config('blog.google_tag_id') }}');</script>
-<!-- End Google Tag Manager -->

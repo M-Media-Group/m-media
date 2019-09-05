11111ff24b40015460cccb6aa4925951d5a0befe
Michał Wargan <mwargan@Michas-MacBook-Pro.local>
2019-09-05 17:44:18 +0200

    Added settings

---
+++ app/Bot.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Bot extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+
+        'user_id',
+        'alias',
+        'address',
+        'last_ip',
+        'last_internal_ip',
+        'service_title',
+        'georegion',
+        'type',
+        'is_active',
+        'last_contact_at',
+
+    ];
+
+    protected $dates = ['last_contact_at'];
+
+    // protected $appends = ['location'];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+}
---
+++ database/migrations/2019_09_05_152824_create_bots_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bots', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('user_id')->nullable();
+            $table->string('alias')->unique();
+            $table->string('address')->unique();
+            $table->ipAddress('last_ip')->nullable();
+            $table->ipAddress('last_internal_ip')->nullable();
+            $table->string('service_title')->nullable();
+            $table->string('georegion')->nullable();
+            $table->string('type')->nullable();
+            $table->boolean('is_active')->default(0);
+            $table->timestamp('last_contact_at')->useCurrent();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bots');
+    }
+}

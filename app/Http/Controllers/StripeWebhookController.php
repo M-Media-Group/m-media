ed9fbef3f2aad105e3ceaa957efb4b95eef39d8a
Michał Wargan <mwargan@Michas-MacBook-Pro.local>
2019-09-05 21:07:59 +0200

    Added Stripe Webhooks

--- app/Http/Controllers/StripeWebhookController.php
+++ app/Http/Controllers/StripeWebhookController.php
@@ -25,7 +25,82 @@ class StripeWebhookController extends CashierController
      */
     public function handleCustomerUpdated($payload)
     {
-        // Handle The Event
+        if ($payload['data']['object']['phone']) {
+            $user = User::firstOrCreate(['stripe_id' => $payload['data']['object']['id']], [
+                "email" => $payload['data']['object']['email'],
+            ]);
+            $input = array();
+            $input['phonenumber'] = $payload['data']['object']['phone'];
+            $input['country'] = null;
+            $input['language'] = 'en';
+            $input['region'] = null;
+
+            $phoneNumberUtil = \libphonenumber\PhoneNumberUtil::getInstance();
+            $phoneNumberGeocoder = \libphonenumber\geocoding\PhoneNumberOfflineGeocoder::getInstance();
+
+            $validNumber = false;
+            $validNumberForRegion = false;
+            $possibleNumber = false;
+            $isPossibleNumberWithReason = null;
+            $geolocation = null;
+            $phoneNumberToCarrierInfo = null;
+            $timezone = null;
+
+            $phoneNumber = $phoneNumberUtil->parse($input['phonenumber'], $input['country'], null, true);
+            $possibleNumber = $phoneNumberUtil->isPossibleNumber($phoneNumber);
+            $isPossibleNumberWithReason = $phoneNumberUtil->isPossibleNumberWithReason($phoneNumber);
+            $validNumber = $phoneNumberUtil->isValidNumber($phoneNumber);
+            if (!$validNumber) {
+                return response()->json(['Error' => "This is not a valid number"], 422);
+
+            }
+            $validNumberForRegion = $phoneNumberUtil->isValidNumberForRegion($phoneNumber, $input['country']);
+            $phoneNumberRegion = $phoneNumberUtil->getRegionCodeForNumber($phoneNumber);
+            $phoneNumberType = $phoneNumberUtil->getNumberType($phoneNumber);
+            $geolocation = $phoneNumberGeocoder->getDescriptionForNumber(
+                $phoneNumber,
+                $input['language'],
+                $input['region']
+            );
+
+            $e164 = $phoneNumberUtil->format($phoneNumber, \libphonenumber\PhoneNumberFormat::E164);
+            $nationalNumber = $phoneNumberUtil->format($phoneNumber, \libphonenumber\PhoneNumberFormat::NATIONAL);
+
+            $phoneNumberToCarrierInfo = \libphonenumber\PhoneNumberToCarrierMapper::getInstance()->getNameForNumber(
+                $phoneNumber,
+                $input['language']
+            );
+            $timezone = \libphonenumber\PhoneNumberToTimeZonesMapper::getInstance()->getTimeZonesForNumber($phoneNumber);
+
+            $countryCode = $phoneNumberUtil->getCountryCodeForRegion($phoneNumberRegion);
+            $country = Country::firstOrCreate(
+                ['iso' => $phoneNumberRegion],
+                ['name' => $geolocation, 'calling_code' => $countryCode]
+            );
+            $phone = Phone::firstOrCreate(
+                ['e164' => $e164],
+                [
+                    'number' => $nationalNumber,
+                    #   'possibleNumber' => $possibleNumber,
+                    #   'validNumber' => $validNumber,
+                    #    'validNumberForRegion' => $validNumberForRegion,
+                    'number_type' => $phoneNumberType,
+                    'country_id' => $country->id,
+                    'timezone' => $timezone[0],
+                    'description' => $phoneNumberToCarrierInfo,
+                    'user_id' => $user->id,
+                    'is_public' => 0,
+                ]
+            );
+        } else {
+            $phone->id = null;
+        }
+        $user = User::updateOrCreate(['stripe_id' => $payload['data']['object']['id']], [
+            "email" => $payload['data']['object']['email'],
+            "phone" => $phone->id,
+        ]);
+
+        return response('Webhook Handled', 200);
     }
 
     /**
@@ -47,7 +122,82 @@ class StripeWebhookController extends CashierController
      */
     public function handleCustomerCreated($payload)
     {
-        // Handle The Event
+        if ($payload['data']['object']['phone']) {
+            $user = User::firstOrCreate(['stripe_id' => $payload['data']['object']['id']], [
+                "email" => $payload['data']['object']['email'],
+            ]);
+            $input = array();
+            $input['phonenumber'] = $payload['data']['object']['phone'];
+            $input['country'] = null;
+            $input['language'] = 'en';
+            $input['region'] = null;
+
+            $phoneNumberUtil = \libphonenumber\PhoneNumberUtil::getInstance();
+            $phoneNumberGeocoder = \libphonenumber\geocoding\PhoneNumberOfflineGeocoder::getInstance();
+
+            $validNumber = false;
+            $validNumberForRegion = false;
+            $possibleNumber = false;
+            $isPossibleNumberWithReason = null;
+            $geolocation = null;
+            $phoneNumberToCarrierInfo = null;
+            $timezone = null;
+
+            $phoneNumber = $phoneNumberUtil->parse($input['phonenumber'], $input['country'], null, true);
+            $possibleNumber = $phoneNumberUtil->isPossibleNumber($phoneNumber);
+            $isPossibleNumberWithReason = $phoneNumberUtil->isPossibleNumberWithReason($phoneNumber);
+            $validNumber = $phoneNumberUtil->isValidNumber($phoneNumber);
+            if (!$validNumber) {
+                return response()->json(['Error' => "This is not a valid number"], 422);
+
+            }
+            $validNumberForRegion = $phoneNumberUtil->isValidNumberForRegion($phoneNumber, $input['country']);
+            $phoneNumberRegion = $phoneNumberUtil->getRegionCodeForNumber($phoneNumber);
+            $phoneNumberType = $phoneNumberUtil->getNumberType($phoneNumber);
+            $geolocation = $phoneNumberGeocoder->getDescriptionForNumber(
+                $phoneNumber,
+                $input['language'],
+                $input['region']
+            );
+
+            $e164 = $phoneNumberUtil->format($phoneNumber, \libphonenumber\PhoneNumberFormat::E164);
+            $nationalNumber = $phoneNumberUtil->format($phoneNumber, \libphonenumber\PhoneNumberFormat::NATIONAL);
+
+            $phoneNumberToCarrierInfo = \libphonenumber\PhoneNumberToCarrierMapper::getInstance()->getNameForNumber(
+                $phoneNumber,
+                $input['language']
+            );
+            $timezone = \libphonenumber\PhoneNumberToTimeZonesMapper::getInstance()->getTimeZonesForNumber($phoneNumber);
+
+            $countryCode = $phoneNumberUtil->getCountryCodeForRegion($phoneNumberRegion);
+            $country = Country::firstOrCreate(
+                ['iso' => $phoneNumberRegion],
+                ['name' => $geolocation, 'calling_code' => $countryCode]
+            );
+            $phone = Phone::firstOrCreate(
+                ['e164' => $e164],
+                [
+                    'number' => $nationalNumber,
+                    #   'possibleNumber' => $possibleNumber,
+                    #   'validNumber' => $validNumber,
+                    #    'validNumberForRegion' => $validNumberForRegion,
+                    'number_type' => $phoneNumberType,
+                    'country_id' => $country->id,
+                    'timezone' => $timezone[0],
+                    'description' => $phoneNumberToCarrierInfo,
+                    'user_id' => $user->id,
+                    'is_public' => 0,
+                ]
+            );
+        } else {
+            $phone->id = null;
+        }
+        $user = User::updateOrCreate(['stripe_id' => $payload['data']['object']['id']], [
+            "email" => $payload['data']['object']['email'],
+            "phone" => $phone->id,
+        ]);
+
+        return response('Webhook Handled', 200);
     }
 
     /**

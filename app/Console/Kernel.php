8fa41e4eb333f0ed1e79534fd02ca5478bde1b38
Michał Wargan <mwargan@Michas-MacBook-Pro.local>
2019-09-06 02:50:34 +0200

    Added Stripe Webhooks

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Jobs\SyncBots;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -27,6 +28,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //     ->everyMinute();
         $schedule->command('telescope:prune')->everyMinute();
+        $schedule->job(new SyncBots)->twiceDaily(1, 13);
     }
 
     /**

a0c386b8a1b7d5a89f7ad506cfe883fc00e629bb
Michał Wargan <mwargan@Michas-MacBook-Pro.local>
2019-09-21 17:29:55 +0200

    Added contact-us widget only to contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,5 +1,32 @@
 @extends('layouts.clean', ['background_image' => "/images/background.jpg"])
 
+@section('opening_body')
+ <!-- Load Facebook SDK for JavaScript -->
+    <div id="fb-root"></div>
+    <script>
+    window.fbAsyncInit = function() {
+      FB.init({
+        xfbml            : true,
+        version          : 'v4.0'
+      });
+    };
+
+    (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
+    <!-- Your customer chat code -->
+    <div class="fb-customerchat"
+        attribution=setup_tool
+        page_id="1640199049612220"
+  theme_color="#eb4647">
+    </div>
+@endsection
+
 @section('above_container')
 <div class="header-section" style="background:#246EBA;">
     <h1>{{ __('Contact us') }}</h1>

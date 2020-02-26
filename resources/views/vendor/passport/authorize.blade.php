b5597f5ed34e13c948c777d70630f2f9a2472516
Michał Wargan <mwargan@Michas-MacBook-Pro.local>
2020-02-26 11:37:59 +0100

    Updated auth screen

--- resources/views/vendor/passport/authorize.blade.php
+++ resources/views/vendor/passport/authorize.blade.php
@@ -2,30 +2,30 @@
 
 @section('above_container')
 <div class="header-section u-bg-primary">
-    <h1>{{ $client->name }}</strong> is requesting permission to access your {{ config('app.name') }} account.</h1>
+    <h1>{{ $client->name }}</strong> is requesting access your {{ config('app.name') }} account.</h1>
 </div>
 <div class="action-section container" data-aos="fade">
     <div class="mt-5">
           <!-- Scope List -->
                         @if (count($scopes) > 0)
                             <div class="scopes">
-                                    <p><strong>This application will be able to:</strong></p>
+                                    <p>{{ $client->name }} will have permission to:</p>
 
                                     <ul>
                                         @foreach ($scopes as $scope)
-                                            <li>{{ $scope->description }}</li>
+                                            <li><strong>{{ $scope->description }}</strong></li>
                                         @endforeach
                                     </ul>
                             </div>
                         @else
                         <div class="scopes">
-                            <p><strong>This application will be able to access all your data and perform operations on your behalf.</strong></p>
+                            <p><strong>{{ $client->name }} will have permission to access all your {{ config('app.name') }} data and perform operations on your behalf.</strong></p>
                         </div>
                         @endif
 
                         <div class="buttons">
                             <!-- Authorize Button -->
-                            <form method="post" action="{{ route('passport.authorizations.approve') }}">
+                            <form method="post" action="{{ route('passport.authorizations.approve') }}" style="display:inline-block;">
                                 {{ csrf_field() }}
 
                                 <input type="hidden" name="state" value="{{ $request->state }}">
@@ -33,15 +33,21 @@
                                 <button type="submit" class="button button-secondary">Authorize</button>
                             </form>
                              <!-- Cancel Button -->
-                            <form method="post" action="{{ route('passport.authorizations.deny') }}">
+                            <form method="post" action="{{ route('passport.authorizations.deny') }}" style="display:inline-block;">
                                 {{ csrf_field() }}
                                 {{ method_field('DELETE') }}
 
                                 <input type="hidden" name="state" value="{{ $request->state }}">
                                 <input type="hidden" name="client_id" value="{{ $client->id }}">
-                                <button class="button">Cancel</button>
+                                <button class="button">Deny</button>
                             </form>
                         </div>
+                        @if($client->client_id)
+                            @php
+                                $user = App\User::find($client->client_id);
+                            @endphp
+                            <p class="mt-3 text-muted small">{{ $client->name }} is owned by M Media customer <strong>{{ $user->name }} {{$user->surname[0]}}</strong>. Only allow applications from sources you trust to access your data.</p>
+                        @endif
     </div>
 </div>
 @endsection

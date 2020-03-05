3e306c431e4fc57e7a1e9b36d32f3c2648688cde
Michał Wargan <mwargan@Michas-MacBook-Pro.local>
2020-03-05 21:17:04 +0100

    Updated file updaupdating logic

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -117,13 +117,13 @@ class FileController extends Controller
 
         $file->update($request->only('is_public', 'user_id'));
 
-        if ($request->is_public) {
+        if (isset($request->is_public)) {
             Storage::setVisibility($file->getOriginal('url'), $request->is_public ? 'public' : 'private');
         }
 
         if ($request->user_id && $request->user_id !== $current_file_user) {
             $hash = Str::random(40);
-            $new_url = 'files/'.($request->user_id ?? 'default').'/'.$hash.'.'.$file->extension;
+            $new_url = 'files/' . ($request->user_id ?? 'default') . '/' . $hash . '.' . $file->extension;
             $path = Storage::move($file->getOriginal('url'), $new_url);
             $file->update(['url' => $new_url]);
         }

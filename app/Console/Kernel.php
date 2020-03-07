248b710f117223837b7735583ddf51a69fd2f8b3
mwargan <mwargan@gmail.com>
2020-03-07 17:41:33 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -34,7 +34,6 @@ class Kernel extends ConsoleKernel
         $schedule->job(new SyncBots())->hourly()->between('7:00', '23:00');
         $schedule->job(new ScrapeInstagramAccounts())->dailyAt('23:00');
         $schedule->job(new LinkEmailsToPrimaryOwners())->dailyAt('24:00');
-
     }
 
     /**
@@ -44,7 +43,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__ . '/Commands');
+        $this->load(__DIR__.'/Commands');
 
         require base_path('routes/console.php');
     }
--- app/Jobs/LinkEmailsToPrimaryOwners.php
+++ app/Jobs/LinkEmailsToPrimaryOwners.php
@@ -39,19 +39,16 @@ class LinkEmailsToPrimaryOwners implements ShouldQueue
             $users = \App\User::with('emails', 'primaryEmail')->get();
             foreach ($users as $user) {
                 //dd();
-                if (!$user->primaryEmail) {
-
+                if (! $user->primaryEmail) {
                     $email = SaveEmail::dispatch(['email' => $user->email], true, $user);
-
-                } elseif (!$user->emails->contains('email', '=', $user->primaryEmail->email) && $user->email_verified_at) {
-
+                } elseif (! $user->emails->contains('email', '=', $user->primaryEmail->email) && $user->email_verified_at) {
                     $email = SaveEmail::dispatch(['email' => $user->primaryEmail->email], true, $user);
                     if ($email['user_id'] !== $user->id) {
                         $email->update(['user_id' => $user->id]);
                     }
-
                 }
             }
+
             return true;
         } catch (Exception $e) {
             return $e;

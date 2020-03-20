c518ab90c49a416501e64f372ac0e9556ee18c9d
Michał Wargan <mwargan@Michas-MacBook-Pro.local>
2020-03-20 17:24:11 +0100

    Added COVID watch

--- resources/views/covid.blade.php
+++ resources/views/covid.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.clean')
 
+@section('title', "Coronavirus COVID-19 tracking")
+@section('meta_description', "Hourly updates on the global Coronavirus COVID-19 epidemic." )
+
 @section('above_container')
     <script type="application/ld+json">
     {
@@ -21,7 +24,15 @@
 
         <div class="header-section" >
             <h1 class="header-section-title">There's <span class="text-danger">{{number_format($cases['Global']['All']['confirmed'])}}</span> confirmed cases of Coronavirus around the world today.</h1>
-            <p class="mb-0" data-aos="fade" data-aos-delay="300">That's just the cases of Coronavirus that have been confirmed in a laboratory. With news of sketchy reporting and others staying at home, there's more cases out there.</p>
+            <p data-aos="fade" data-aos-delay="300">Since yesterday, that's an increase of {{ number_format(
+                $cases['Global']['All']['confirmed'] -
+                ($history['Global']['All']['dates'][Carbon\Carbon::now()->subDays(1)->toDateString()]))
+            }} cases, or {{ number_format(
+                ($cases['Global']['All']['confirmed'] -
+                $history['Global']['All']['dates'][Carbon\Carbon::now()->subDays(1)->toDateString()]) /
+                $cases['Global']['All']['confirmed'] * 100
+            ) }}%.</p>
+            <p class="mb-0" data-aos="fade" data-aos-delay="300">Only cases tested in a laboratory are counted; with news of sketchy reporting and others staying at home, there's more cases out there.</p>
 {{--             <a class="button button-secondary mt-3 mb-5" href="/contact" data-aos="fade" data-aos-delay="500">Talk to an expert now</a>
             <a class="button button-secondary text-white d-none" href="/case-studies">See case studies</a> --}}
         </div>
@@ -75,7 +86,12 @@
     <div class="header-section row m-0" style="background: var(--white);">
         <div class="col-md-6" data-aos="fade">
             <h3 class="mt-0" data-aos="fade">France is dealing with <span class="text-danger">{{number_format($cases['France']['All']['confirmed'] - $cases['France']['All']['deaths'] - $cases['France']['All']['recovered'])}}</span> active cases.</h3>
-            <p data-aos="fade">{{number_format(($cases['France']['All']['confirmed']) / $cases['France']['All']['population'] *100, 2)}}% of the population have or have had the Coronavirus. More unreported people are likely to be sick at home.</p>
+            <p data-aos="fade">{{number_format(($cases['France']['All']['confirmed']) / $cases['France']['All']['population'] *100, 2)}}% of the population have or have had the Coronavirus, up from last week by {{ number_format(
+                ($cases['France']['All']['confirmed'] -
+                $history['France']['All']['dates'][Carbon\Carbon::now()->subWeeks(1)->toDateString()]) /
+                $history['France']['All']['dates'][Carbon\Carbon::now()->subWeeks(1)->toDateString()] * 100
+            ) }}%. More unreported people are likely to be sick at home.</p>
+            <p>The mainland accounts for {{number_format(($cases['France']['France']['confirmed']) / $cases['France']['All']['confirmed'] *100, 2)}}% of all confirmed cases in France.</p>
         </div>
         <div class="col-md-6 mb-5 flex">
             <div class="flex" style="flex-wrap: wrap;">
@@ -90,7 +106,13 @@
 
             <p data-aos="fade">That's {{number_format(($cases['Italy']['All']['confirmed'] - $cases['Italy']['All']['deaths'] - $cases['Italy']['All']['recovered']) / $cases['Italy']['All']['population'] *100, 2)}}% of the population in hospital right now because of the Coronavirus. More unreported people are likely to be sick at home.</p>
 
-            <p data-aos="fade">Italy has an estimated {{number_format($cases['Italy']['All']['population']/1000*3.18)}} hospital beds that are usually 78.9% occupied. That leaves just {{number_format(($cases['Italy']['All']['population']/1000*3.18) - ($cases['Italy']['All']['population']/1000*3.18 * 0.78))}} hospital beds to deal with the Coronavirus, of which an estimated {{number_format(($cases['Italy']['All']['confirmed'] - $cases['Italy']['All']['deaths'] - $cases['Italy']['All']['recovered']) / (($cases['Italy']['All']['population']/1000*3.18) - ($cases['Italy']['All']['population']/1000*3.18 * 0.78)) * 100)}}% are already in use for this exact purpose.</p>
+            <p data-aos="fade">According to the OECD, Italy has an estimated {{number_format(192548)}} hospital beds that are usually 78.9% occupied. That leaves just {{number_format(($cases['Italy']['All']['population']/1000*3.18) - ($cases['Italy']['All']['population']/1000*3.18 * 0.78))}} hospital beds to deal with the Coronavirus, of which an estimated {{number_format(($cases['Italy']['All']['confirmed'] - $cases['Italy']['All']['deaths'] - $cases['Italy']['All']['recovered']) / (($cases['Italy']['All']['population']/1000*3.18) - ($cases['Italy']['All']['population']/1000*3.18 * 0.78)) * 100)}}% are already in use for this exact purpose.</p>
+
+            <p>Since last week, there's been a {{ number_format(
+                ($cases['Italy']['All']['confirmed'] -
+                $history['Italy']['All']['dates'][Carbon\Carbon::now()->subWeeks(1)->toDateString()]) /
+                $history['Italy']['All']['dates'][Carbon\Carbon::now()->subWeeks(1)->toDateString()] * 100
+            ) }}% increase in reported cases.</p>
         </div>
         <div class="col-md-6 mb-5 flex">
             <div class="flex" style="flex-wrap: wrap;">
@@ -102,8 +124,8 @@
     <div class="header-section row m-0" style="background:var(--white);">
         <div class="col-md-6" data-aos="fade">
             <h3 class="mt-0" data-aos="fade">China, where the outbreak is thought to have started, now has <span class="text-danger">{{number_format($cases['China']['All']['confirmed'] - $cases['China']['All']['deaths'] - $cases['China']['All']['recovered'])}}</span> active cases.</h3>
-            <p data-aos="fade">To date, China has had {{number_format($cases['China']['All']['confirmed'])}} confirmed cases of the Coronavirus.</p>
-            <p>Some sources report that data from China may not be 100% accurate and estimate the actual figure to be higher.</p>
+            <p data-aos="fade">To date, China has had {{number_format($cases['China']['All']['confirmed'])}} confirmed cases of the Coronavirus, accounting for {{number_format($cases['China']['All']['confirmed'] / ($cases['Global']['All']['confirmed']) * 100)}}% of the global cases.</p>
+            <p>Some sources report that data from China may not be 100% accurate and estimate the actual figures to be higher.</p>
         </div>
         <div class="col-md-6 mb-5 flex">
             <div class="flex" style="flex-wrap: wrap;">
@@ -116,6 +138,11 @@
         <div class="col-md-6" data-aos="fade">
             <h3 class="mt-0" data-aos="fade">Spain has <span class="text-danger">{{number_format($cases['Spain']['All']['confirmed'])}}</span> confirmed cases.</h3>
             <p style="max-width: 550px;" data-aos="fade">{{number_format($cases['Spain']['All']['recovered'])}} have recovered while {{number_format($cases['Spain']['All']['deaths'])}} have died. Spain now accounts for {{number_format($cases['Spain']['All']['confirmed'] / ($cases['Global']['All']['confirmed'] - $cases['China']['All']['confirmed']) * 100)}}% of the global cases (excluding China).</p>
+            <p>In the past week, Spain has seen a {{ number_format(
+                ($cases['Spain']['All']['confirmed'] -
+                $history['Spain']['All']['dates'][Carbon\Carbon::now()->subWeeks(1)->toDateString()]) /
+                $history['Spain']['All']['dates'][Carbon\Carbon::now()->subWeeks(1)->toDateString()] * 100
+            ) }}% increase in reported cases.</p>
         </div>
         <div class="col-md-6 mb-5 flex">
             <div class="flex" style="flex-wrap: wrap;">
@@ -127,7 +154,7 @@
     <div class="header-section row m-0 u-bg-secondary">
         <div class="col-md-12">
             <h3 class="mt-0" data-aos="fade">Wash your hands.</h3>
-            <p data-aos="fade">Wash your hands regularly to prevent the spread of the disease.</p>
+            <p data-aos="fade">Wash your hands with soap regularly to prevent the spread of the disease.</p>
             <a class="button button-primary u-center" href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public" rel="noopener" data-aos="fade">Read the WHO recommendations</a>
         </div>
     </div>
@@ -135,7 +162,7 @@
     <div class="header-section u-bg-primary row m-0">
         <div class="col-md-12 text-center">
             <div class="flex" style="flex-wrap: wrap; flex-direction: column;">
-                <h3 class="mt-0" data-aos="fade">This live data was pulled from our COVID-19 API</h3>
+                <h3 class="mt-0" data-aos="fade">This live data was pulled from our public COVID-19 API</h3>
                 <p data-aos="fade" data-aos-delay="300">Using information from Johns Hopkins University</p>
                 <p data-aos="fade" data-aos-delay="400" class="small">Data updated every hour</p>
                 <a class="button button-secondary" href="https://blog.mmediagroup.fr/post/m-media-launches-covid-19-api/" rel="noopener" data-aos="fade">Read about the API</a>
--- routes/web.php
+++ routes/web.php
@@ -124,7 +124,7 @@ Route::group(['middleware' => ['verified']], function () {
     });
 
     Route::get('/my-account/billing', function () {
-        return Redirect::to('/users/'.Auth::id().'/billing', 301);
+        return Redirect::to('/users/' . Auth::id() . '/billing', 301);
     });
 
     Route::get('/domains/check-availability', function () {
@@ -154,6 +154,7 @@ Route::get('/covid-19', function () {
     $client = new \GuzzleHttp\Client();
     $request = $client->get('https://covid-api.mmediagroup.fr/v1/cases');
     $response = json_decode($request->getBody(), true);
-
-    return View::make('covid', ['cases' => $response]);
+    $request = $client->get('https://covid-api.mmediagroup.fr/v1/history?status=Confirmed');
+    $response_2 = json_decode($request->getBody(), true);
+    return View::make('covid', ['cases' => $response, 'history' => $response_2]);
 });

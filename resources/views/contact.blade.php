6936e1b86e85c2c953f7fc5acb6192149b22d941
Michał Wargan <mwargan@michas-macbook-pro.home>
2020-04-21 13:47:26 +0200

    Removed Messenger on contact page for non-logged in users

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,35 +3,37 @@
 @section('meta_description', "Contact us at ".config('app.name')." via email, phone, or Messenger and let's get talking about your business marketing needs." )
 
 
-@section('opening_body')
- <!-- Load Facebook SDK for JavaScript -->
-    <div id="fb-root"></div>
-    <script>
-    window.fbAsyncInit = function() {
-      FB.init({
-        xfbml            : true,
-        version          : 'v4.0'
-      });
-    };
+@auth
+    @section('opening_body')
+     <!-- Load Facebook SDK for JavaScript -->
+        <div id="fb-root"></div>
+        <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v4.0'
+          });
+        };
 
-    (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-    fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));</script>
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
 
-    <!-- Your customer chat code -->
-    <div class="fb-customerchat"
-        attribution=setup_tool
-        page_id="1640199049612220"
-	  theme_color="#eb4647"
-	  greeting_dialog_display="show"
-	  logged_out_greeting="Chat with us using Facebook Messenger!"
-	  logged_in_greeting="Hey there! How can we help?">
-    </div>
-@endsection
+        <!-- Your customer chat code -->
+        <div class="fb-customerchat"
+            attribution=setup_tool
+            page_id="1640199049612220"
+    	  theme_color="#eb4647"
+    	  greeting_dialog_display="show"
+    	  logged_out_greeting="Chat with us using Facebook Messenger!"
+    	  logged_in_greeting="Hey there! How can we help?">
+        </div>
+    @endsection
+@endauth
 
 @section('above_container')
 <div class="header-section u-bg-primary">

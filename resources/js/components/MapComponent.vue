8a4de04348a6467aaad1056096fe0d02425d11b1
mwargan <mwargan@gmail.com>
2020-04-27 00:25:19 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- resources/js/components/MapComponent.vue
+++ resources/js/components/MapComponent.vue
@@ -1,34 +1,34 @@
 <template>
-  <v-map :zoom="10" :center="initialLocation" style="width:100%;height:500px;">
-    <v-tilelayer url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"></v-tilelayer>
-    <v-locatecontrol/>
-  </v-map>
+    <v-map :zoom="10" :center="initialLocation" style="width: 100%; height: 500px;">
+        <v-tilelayer url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"></v-tilelayer>
+        <v-locatecontrol />
+    </v-map>
 </template>
 
 <script>
-import { LMap, LTileLayer } from "vue2-leaflet";
+import { LMap, LTileLayer } from 'vue2-leaflet';
 import { latLng, Icon, icon } from 'leaflet';
 import Vue2LeafletLocatecontrol from 'vue2-leaflet-locatecontrol/Vue2LeafletLocatecontrol';
 
 export default {
-  components: {
-    "v-map": LMap,
-    "v-tilelayer": LTileLayer,
-    "v-locatecontrol": Vue2LeafletLocatecontrol
-  },
-  data() {
-    return {
-      initialLocation: [59.93428, 30.335098]
-    }
-  }
-}
+    components: {
+        'v-map': LMap,
+        'v-tilelayer': LTileLayer,
+        'v-locatecontrol': Vue2LeafletLocatecontrol,
+    },
+    data() {
+        return {
+            initialLocation: [59.93428, 30.335098],
+        };
+    },
+};
 </script>
 <style>
-@import "~leaflet/dist/leaflet.css";
-@import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
+@import '~leaflet/dist/leaflet.css';
+@import 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css';
 html,
 body {
-  height: 100%;
-  margin: 0;
+    height: 100%;
+    margin: 0;
 }
 </style>

76a509be534838f7db8df44e68436fceaff43628
Michał Wargan <mwargan@Michas-MacBook-Pro.local>
2020-10-07 22:14:27 +0200

    added AdNetworks temp fix

--- config/google-ads.php
+++ config/google-ads.php
@@ -2,17 +2,19 @@
 
 return [
     //Environment=> test/production
-    'env' => 'production',
+    'env' => env('APP_ENV'),
     //Google Ads
+
+    // Issue with caching config, reported here: https://github.com/Edujugon/laravel-google-ads/issues/47
     'production' => [
-        'developerToken' => env('GOOGLE_ADS_DEVELOPER_TOKEN'),
-        'clientCustomerId' => env('GOOGLE_ADS_CUSTOMER_ID'),
+        'developerToken' => env('GOOGLE_ADS_DEVELOPER_TOKEN', config('google-ads.test.developerToken')),
+        'clientCustomerId' => env('GOOGLE_ADS_CUSTOMER_ID', config('google-ads.test.clientCustomerId')),
         'userAgent' => env('APP_NAME'),
-        'clientId' => env('GOOGLE_ADS_CLIENT_ID'),
-        'clientSecret' => env('GOOGLE_ADS_CLIENT_SECRET'),
-        'refreshToken' => env('GOOGLE_ADS_REFRESH_TOKEN'),
+        'clientId' => env('GOOGLE_ADS_CLIENT_ID', config('google-ads.test.clientId')),
+        'clientSecret' => env('GOOGLE_ADS_CLIENT_SECRET', config('google-ads.test.clientSecret')),
+        'refreshToken' => env('GOOGLE_ADS_REFRESH_TOKEN', config('google-ads.test.refreshToken')),
     ],
-    'test' => [
+    'local' => [
         'developerToken' => env('GOOGLE_ADS_DEVELOPER_TOKEN'),
         'clientCustomerId' => env('GOOGLE_ADS_CUSTOMER_ID'),
         'userAgent' => env('APP_NAME'),

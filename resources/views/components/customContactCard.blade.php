1c8a73b1b561f4a91cc3b40e101cad4945f2a4f6
Michał Wargan <mwargan@gmail.com>
2020-11-23 16:43:54 +0100

    Implemented customer suggestions

--- resources/views/components/customContactCard.blade.php
+++ resources/views/components/customContactCard.blade.php
@@ -1,7 +1,7 @@
 <div class="row m-0 pt-5 pb-5 ">
     <div class="action-section card round-all-round text-center" data-aos="fade" id="{{$id}}">
         @if(isset($image))
-        <img src="{{$image}}" class="card-img-top p-0" style="max-height: 135px;object-fit: scale-down;" alt="{{Config('app.name')}}">
+        <img src="{{$image}}" class="card-img-top p-0" style="max-height: 135px;object-fit: scale-down;margin-bottom:-1rem;" alt="{{Config('app.name')}}">
         @endif
           <div class="card-body">
             <h3 class="card-title">{{$title}}</h3>
